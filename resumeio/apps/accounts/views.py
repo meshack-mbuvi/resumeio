915a30a45e6a068ddbe441141ef4a9eb468cc4f9
Meshack Mbuvi <meshmbuvi@gmail.com>
2020-01-05 17:53:05 +0300

    [feat]  Add app for user authentication

--- resumeio/apps/accounts/views.py
+++ resumeio/apps/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, FormView
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+from django.core.exceptions import ValidationError
+
+class SignUpView(CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = SignUpForm
+
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        
+
+        return render(request, 'accounts/signup.html', {'form':form})
+
+  
--- resumeio/settings.py
+++ resumeio/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'resumeio.apps.accounts'
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'resumeio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,11 +78,13 @@ WSGI_APPLICATION = 'resumeio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
+        'HOST':os.environ.get('DB_HOST','127.0.0.1')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -118,3 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+"""
+Tell Django about the custom `User` model we created. The string
+`authentication.User` tells Django we are referring to the `User` model in
+the `authentication` module. This module is registered above in a setting
+called `INSTALLED_APPS`.
+"""
+AUTH_USER_MODEL = 'accounts.User'
